a53116bb09ffa2043fb635a8f158e8a1c91be579
Alejandro Mora <alejomora1205@gmail.com>
2025-06-29 21:50:34 -0500

    Seccion 12 finalizada, administracion de archivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({ 
+      rootPath: join(__dirname,'..','Public'),    
+    }),
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(
       {
@@ -23,6 +29,7 @@ import { SeedModule } from './seed/seed.module';
     ProductsModule,
     CommonModule,
     SeedModule,
+    FilesModule,
   ],
 })
 export class AppModule {}
